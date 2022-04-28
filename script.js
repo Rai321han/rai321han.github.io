2ba5b6cbf8a67cf6caa4ee24c87dab4af82ddb1a
Raihan Uddin <uddinraihan797@gmail.com>
2022-04-29 02:16:22 +0600

    Responsive navigation-Slide

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ const heroButton = hero.querySelector(".btn");
 heroButton.addEventListener("click", () => {
   document.getElementById("content").scrollIntoView(true);
 });
+
+const menu_btn = document.querySelector(".menu-btn");
+const nav_container = document.querySelector(".nav-container");
+
+menu_btn.addEventListener("click", () => {
+  if (nav_container.getAttribute("target") === "true") {
+    nav_container.setAttribute("target", "false");
+  } else nav_container.setAttribute("target", "true");
+});
