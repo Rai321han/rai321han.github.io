9d5dfac2f7dc83e7ac089b0506acb6ceedc98d80
Raihan Uddin <uddinraihan797@gmail.com>
2022-04-30 00:14:29 +0600

    Resposive Nav Menu Button Complete

--- script.js
+++ script.js
@@ -6,11 +6,33 @@ heroButton.addEventListener("click", () => {
   document.getElementById("content").scrollIntoView(true);
 });
 
-const menu_btn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
 const nav_container = document.querySelector(".nav-container");
+const openbtn = document.getElementById("openSVG");
+const closebtn = document.getElementById("closeSVG");
 
-menu_btn.addEventListener("click", () => {
+const changeButton = function (target) {
+  let state1 = "";
+  let state2 = "";
+  if (target) {
+    state1 = "open";
+    state2 = "close";
+  } else {
+    state1 = "close";
+    state2 = "open";
+  }
+  openbtn.classList.remove(`menu-btn--${state1}`);
+  openbtn.classList.add(`menu-btn--${state2}`);
+  closebtn.classList.remove(`menu-btn--${state2}`);
+  closebtn.classList.add(`menu-btn--${state1}`);
+};
+
+menu.addEventListener("click", (e) => {
   if (nav_container.getAttribute("target") === "true") {
     nav_container.setAttribute("target", "false");
-  } else nav_container.setAttribute("target", "true");
+    changeButton(false);
+  } else {
+    nav_container.setAttribute("target", "true");
+    changeButton(true);
+  }
 });
